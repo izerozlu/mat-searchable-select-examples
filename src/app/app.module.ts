da7d6ebbe23726449c79116701c4bc94e0bc400b
izerozlu <izerozlu@gmail.com>
2020-01-22 22:55:00 +0300

    ValueSetComponent implemented.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,29 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {AppComponent} from './app.component';
-import {ReactiveFormsModule} from '@angular/forms';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {BasicUsageComponent} from './basic-usage/basic-usage.component';
-import {MatSearchableSelectModule} from 'mat-searchable-select';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { BasicUsageComponent } from './components/basic-usage/basic-usage.component';
+import { MatSearchableSelectModule } from 'mat-searchable-select';
+import {MatDividerModule} from '@angular/material/divider';
+import { ValueSetComponent } from './components/value-set/value-set.component';
+import {MatButtonToggleModule} from "@angular/material/button-toggle";
 
 @NgModule({
   declarations: [
     AppComponent,
-    BasicUsageComponent
+    BasicUsageComponent,
+    ValueSetComponent
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
     MatFormFieldModule,
-    MatSearchableSelectModule
+    MatSearchableSelectModule,
+    MatDividerModule,
+    MatButtonToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
